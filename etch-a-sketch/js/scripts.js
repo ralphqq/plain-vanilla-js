c5160941c128be33b43858126ade8e2f32e46239
Ralph Quirequire <ralph.quirequire@outlook.com>
2020-02-01 15:18:43 +0800

    Basic smoke test for mouseenter and mouseleave events

--- etch-a-sketch/js/scripts.js
+++ etch-a-sketch/js/scripts.js
@@ -7,10 +7,29 @@ const nSquares = 16;
 
 console.log(container.style.cssText);
 
-// Append the cells
+// Append the grid items to parent container
 for (let i = 0; i < nSquares ** 2; i++) {
   let cellDiv = document.createElement('div');
   cellDiv.classList.add('grid-item');
+  cellDiv.setAttribute('id', i + 1);
   cellDiv.textContent = i + 1;
   container.appendChild(cellDiv);
 }
+
+// get nodelist of grid items
+const gridItems = document.querySelectorAll('.grid-item');
+
+// set events
+gridItems.forEach(elem => {
+  let elemId = elem.getAttribute('id');
+
+  elem.addEventListener('mouseenter', e => {
+    console.log(`Now entering item ${elemId}`);
+    elem.classList.toggle('hovered');
+  });
+
+  elem.addEventListener('mouseleave', e => {
+    console.log(`Now leaving item ${elemId}`);
+    elem.classList.toggle('hovered');
+  });
+});
